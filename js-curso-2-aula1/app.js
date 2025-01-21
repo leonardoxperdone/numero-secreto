57ce18160b4142fbaa0ed0e636367e1290d67fa9
Leonardo Perdone <leonardo.perdone@gmail.com>
2025-01-20 21:01:30 -0300

    função ler texto desligada

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -3,18 +3,18 @@ let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-function exibirTextoNaTela(tag, texto) {
-  let campo = document.querySelector(tag);
-  campo.innerHTML = texto;
-  if ('speechSynthesis' in window) {
-      let utterance = new SpeechSynthesisUtterance(texto);
-      utterance.lang = 'pt-BR'; 
-      utterance.rate = 1.2; 
-      window.speechSynthesis.speak(utterance); 
-  } else {
-      console.log("Web Speech API não suportada neste navegador.");
-  }
-}
+//function exibirTextoNaTela(tag, texto) {
+//  let campo = document.querySelector(tag);
+//  campo.innerHTML = texto;
+//  if ('speechSynthesis' in window) {
+//      let utterance = new SpeechSynthesisUtterance(texto);
+//      utterance.lang = 'pt-BR';
+//      utterance.rate = 1.2;
+//      window.speechSynthesis.speak(utterance);
+//  } else {
+//      console.log("Web Speech API não suportada neste navegador.");
+//  }
+//}
 
 function exibirMensagemInicial() {
   exibirTextoNaTela("h1", "Jogo do número secreto");
